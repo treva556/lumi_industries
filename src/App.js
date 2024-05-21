2a08e96bb4f6d27ada0af4d8bd0d3e6b4f9c4ee4
treva556 <marktreva92@gmail.com>
2024-05-21 15:52:51 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className=' bg-black h-10'>
+      <p className=' text-white'> X </p>
+       </div>
+       <div className=' bg-yellow-400 h-10'> LUMI INDUSTRIES</div>
+
+     <div className=' bg-yellow-400 h-10'>
+       </div>
+
+     <p className=''> pppp</p>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+     <div className=''>
+
+     </div>
+     <p className=''> pppp</p>
+    </div>
+
+  );
+}
+
+export default Home;
+
+
+
