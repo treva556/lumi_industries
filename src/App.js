30672103848a198181d5bf097e75522cd8def6f3
treva556 <marktreva92@gmail.com>
2024-05-21 16:09:06 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './components/nav';
 
 function App() {
   return (
@@ -7,12 +9,13 @@ function App() {
      <div className=' bg-black h-10'>
       <p className=' text-white'> X </p>
        </div>
-       <div className=' bg-yellow-400 h-10'> LUMI INDUSTRIES</div>
+       <div className=' bg-yellow-400 h-10 text-4xl'> LUMI INDUSTRIES</div>
 
      <div className=' bg-yellow-400 h-10'>
+      <Nav/>
        </div>
+       <div className=' bg-yellow-400 h-2 text-4xl'> </div>
 
-     <p className=''> pppp</p>
     </div>
   );
 }
